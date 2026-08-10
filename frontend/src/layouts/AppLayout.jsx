f8b3a98d67019fae16a177eb978a959479c04ec0
paulvencent1 <Paulvencent103@gmail.com>
2026-08-10 04:37:44 -0700

    fix

--- frontend/src/layouts/AppLayout.jsx
+++ frontend/src/layouts/AppLayout.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
+
 import Sidebar from '../components/layout/Sidebar';
 import Topbar from '../components/layout/Topbar';
 import useLowStockAlerts from '../hooks/useLowStockAlerts';
